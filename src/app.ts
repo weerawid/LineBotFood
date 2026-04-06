c342c135755e954885494201dcd03ec23681a437
w.nitinunchaiyakul <osacn_weerawid.nitinunchaiyakul@krungthai.com>
2026-04-07 00:32:09 +0700

    update secret

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { logger } from './core/middleware/logger.js';
 
 const app = express();
 const middleWareLine = middleware({
- channelSecret: process.env.LINE_CHANNEL_SECRET ?? ""
+ channelSecret: '331e81c30fc0127ab0298be36d5fae4e'
 })
 
 app.post("/webhook", middleWareLine, helperContext, logger, webhook);
